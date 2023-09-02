5edb8c3bc64b6c5eee2cb2c101d3d0a7294f2300
Alex-Gois <alex.oli.gois@gmail.com>
2023-09-02 14:04:14 -0300

    Initial Commit

---
+++ TableViewNoViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewNoViewCode
+//
+//  Created by Alex Gois on 02/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
